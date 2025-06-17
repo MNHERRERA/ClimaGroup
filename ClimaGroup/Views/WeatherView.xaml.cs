1324d6f286a38bdf624b08a73e068d19a59755f5
MateoHerrera <mateo.herrera@udla.edu.ec>
2025-06-17 08:31:05 -0500

    Correc

---
+++ ClimaGroup/Views/WeatherView.xaml.cs
@@ -0,0 +1,12 @@
+using ClimaGroup.ViewModels;
+
+namespace ClimaGroup.Views;
+
+public partial class WeatherView : ContentPage
+{
+	public WeatherView()
+	{
+		InitializeComponent();
+		//BindingContext = new WeatherViewModel();
+    }
+}
