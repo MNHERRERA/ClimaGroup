7d0917af0c33aa2af537653325ef761eb99ac5ca
laboratorio <laboratorio@UPOAULA10717>
2025-06-17 07:29:43 -0500

    Agregar archivos de proyecto.

---
+++ ClimaGroup/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ClimaGroup
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ClimaGroup/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ClimaGroup
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
