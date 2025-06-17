476c96e84e679ebecf939c74cdc507a7dc9bf6d3
MateoHerrera <mateo.herrera@udla.edu.ec>
2025-06-17 08:15:40 -0500

    sexo

---
+++ ClimaGroup/Repositories/WeatherRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using ClimaGroup.Models;
+using ClimaGroup.Repositories;
+
+namespace ClimaGroup.Repositories
+{
+    class WeatherRepository
+    {
+        public async Task<WeatherData> GetWeatherCurrentLocationAsync()
+        {
+            GeolocationRepository geolocationRepository = new GeolocationRepository();
+            var location = await geolocationRepository.GetCurrentLocation();
+            return await GetWeatherDataAsync(location.Latitude, location.Longitude);
+        }
+
+        public async Task<WeatherData> GetWeatherDataAsync(double latitude, double longitude)
+        {
+            HttpClient httpClient = new HttpClient();
+            string url = "https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longitude;
+
+            var response = await httpClient.GetAsync(url);
+            var result = await response.Content.ReadAsStringAsync();
+            WeatherData data = JsonConvert.DeserializeObject<WeatherData>(result);
+            return data;
+        }
+    }
+
+}
---
+++ ClimaGroup/ViewModels/WeatherViewModel.cs
@@ -0,0 +1,43 @@
+﻿using ClimaGroup.Models;
+using ClimaGroup.Repositories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClimaGroup.ViewModels
+{
+    class WeatherViewModel
+    {
+        private WeatherData _weatherData;
+        public WeatherData WeatherData
+        {
+            get => _weatherData;
+            set
+            {
+                if (_weatherData != value)
+                {
+                    _weatherData = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public WeatherViewModel()
+        {
+            _weatherData = new WeatherData(); // Initialize with an empty WeatherData object
+            GetCurrentWeatherAsync();  
+
+        }
+        public async Task GetCurrentWeatherAsync()
+        {
+            WeatherRepository weatherRepository = new WeatherRepository();
+            WeatherData = await weatherRepository.GetWeatherCurrentLocationAsync();
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string name = "") => 
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
